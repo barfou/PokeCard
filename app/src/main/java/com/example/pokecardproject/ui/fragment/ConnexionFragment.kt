f94c82cebacf172b9186b8c151cbe89aec74a39e
PaulFgx <pfourgoux@live.fr>
2020-01-28 15:53:57 +0100

    add connexion fragment xml and animation

--- app/src/main/java/com/example/pokecardproject/ui/fragment/ConnexionFragment.kt
+++ app/src/main/java/com/example/pokecardproject/ui/fragment/ConnexionFragment.kt
@@ -5,13 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.example.pokecardproject.R
-import kotlinx.android.synthetic.main.fragment_choice_login.*
 
-// log with sqlite : https://www.youtube.com/watch?v=1WPAXHhG6u0
-
-class ChoiceLoginFragment : Fragment() {
+class ConnexionFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,17 +18,10 @@ class ChoiceLoginFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_choice_login, container, false)
+        return inflater.inflate(R.layout.fragment_connexion, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        btn_new_user.setOnClickListener {
-            findNavController().navigate(R.id.action_choice_login_fragment_to_registration_fragment)
-        }
-        btn_connexion.setOnClickListener {
-            findNavController().navigate(R.id.action_choice_login_fragment_to_connexion_fragment)
-        }
     }
 }
--- app/src/main/java/com/example/pokecardproject/utils/Utils.kt
+++ app/src/main/java/com/example/pokecardproject/utils/Utils.kt
@@ -1,8 +1,12 @@
 package com.example.pokecardproject.utils
 
+import android.content.Context
 import android.view.MotionEvent
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.SeekBar
+import androidx.core.content.ContextCompat.getSystemService
+
 
 class Utils {
 
