93a99fba3eef2434395b3f0f1223e3c14a351743
FOURGOUX PAUL p0701968 <paul.fourgoux@etu.univ-lyon1.fr>
2020-02-18 10:36:15 +0100

    update fragment_add_pokemon

--- app/src/main/java/com/example/pokecardproject/utils/Utils.kt
+++ app/src/main/java/com/example/pokecardproject/utils/Utils.kt
@@ -1,22 +1,17 @@
 package com.example.pokecardproject.utils
 
-import android.content.Context
 import android.view.MotionEvent
 import android.view.View
-import android.view.inputmethod.InputMethodManager
 import android.widget.SeekBar
-import androidx.core.content.ContextCompat.getSystemService
 
 
-class Utils {
+object Utils {
 
-    companion object {
-        fun lockSeekBar(seekBar: SeekBar) {
-            seekBar.setOnTouchListener(object : View.OnTouchListener {
-                override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-                    return true
-                }
-            })
-        }
+    fun lockSeekBar(seekBar: SeekBar) {
+        seekBar.setOnTouchListener(object : View.OnTouchListener {
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                return true
+            }
+        })
     }
 }
