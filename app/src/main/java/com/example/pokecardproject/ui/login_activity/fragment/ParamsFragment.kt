da118965fe6ec1da7b5acd691efef9cc6663a557
FOURGOUX PAUL p0701968 <paul.fourgoux@etu.univ-lyon1.fr>
2020-02-21 12:22:46 +0100

    add params fragment

--- app/src/main/java/com/example/pokecardproject/ui/login_activity/fragment/ParamsFragment.kt
+++ app/src/main/java/com/example/pokecardproject/ui/login_activity/fragment/ParamsFragment.kt
@@ -1,15 +1,15 @@
 package com.example.pokecardproject.ui.login_activity.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.pokecardproject.R
 import kotlinx.android.synthetic.main.fragment_choice_login.*
 
-class ChoiceLoginFragment : Fragment() {
+class ParamsFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,17 +20,10 @@ class ChoiceLoginFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_choice_login, container, false)
+        return inflater.inflate(R.layout.fragment_params, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        btn_new_user.setOnClickListener {
-            findNavController().navigate(R.id.action_choice_login_fragment_to_registration_fragment)
-        }
-        btn_connexion.setOnClickListener {
-            findNavController().navigate(R.id.action_choice_login_fragment_to_connexion_fragment)
-        }
     }
 }
