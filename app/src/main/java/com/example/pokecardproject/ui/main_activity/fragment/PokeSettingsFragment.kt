7e069376ecfbf2f9e218f95f0ea263737bf874c8
PaulFgx <pfourgoux@live.fr>
2020-03-01 11:36:22 +0100

    load user on view created

--- app/src/main/java/com/example/pokecardproject/ui/main_activity/fragment/PokeSettingsFragment.kt
+++ app/src/main/java/com/example/pokecardproject/ui/main_activity/fragment/PokeSettingsFragment.kt
@@ -5,12 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import com.example.pokecardproject.R
+import com.example.pokecardproject.ui.viewmodel.MainActivityViewModel
+import kotlinx.android.synthetic.main.fragment_pokesettings.*
 
 class PokeSettingsFragment : Fragment() {
 
+    private lateinit var mainActivityViewModel: MainActivityViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        activity?.run {
+            mainActivityViewModel = ViewModelProvider(this, MainActivityViewModel).get()
+        }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -19,5 +29,16 @@ class PokeSettingsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        loadCurrentUser()
+    }
+
+    private fun loadCurrentUser() {
+
+        mainActivityViewModel.currentUser?.run {
+            edt_login.setText(this.login)
+            edt_mail.setText(this.mail)
+            edt_password.setText(this.password)
+        }
     }
 }
