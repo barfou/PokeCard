3f8715affd6a8c763c4e56cd8a7e77e8cd84ad64
PaulFgx <pfourgoux@live.fr>
2020-03-04 19:07:36 +0100

    fix index out of bounds exception

--- app/src/main/java/com/example/pokecardproject/ui/widget/holder/PokemonDBViewHolder.kt
+++ app/src/main/java/com/example/pokecardproject/ui/widget/holder/PokemonDBViewHolder.kt
@@ -11,6 +11,7 @@ import com.example.pokecardproject.data.model.Competence
 import com.example.pokecardproject.data.model.PokemonBase
 import com.example.pokecardproject.data.model.PokemonDB
 import com.example.pokecardproject.data.networking.BaseUrlHolder
+import com.example.pokecardproject.utils.lockSeekBar
 import kotlinx.android.synthetic.main.holder_pokemon.view.*
 import kotlinx.android.synthetic.main.holder_pokemon_db.view.*
 
@@ -20,17 +21,26 @@ class PokemonDBViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
              position: Int) {
 
         itemView.tv_nom.text = pokemonDB.nom
-        itemView.tv_taille.text = pokemonDB.taille.toString()
-        itemView.tv_poids.text = pokemonDB.poids.toString()
+        itemView.tv_taille.text = "Taille : " + pokemonDB.taille.toString()
+        itemView.tv_poids.text = "Poids : " + pokemonDB.poids.toString()
 
-        itemView.tv_competence_1.text = pokemonDB.competences[0]?.nom
-        itemView.tv_competence_2.text = pokemonDB.competences[1]?.nom
-        itemView.tv_competence_3.text = pokemonDB.competences[2]?.nom
+        // To avoid IndexOutOfBoundsException
+        if (pokemonDB.competences.isNotEmpty()) {
+        itemView.tv_competence_1.text = "‣ " + pokemonDB.competences[0]?.nom }
+        if (pokemonDB.competences.size > 1) {
+        itemView.tv_competence_2.text = "‣ " + pokemonDB.competences[1]?.nom }
+        if (pokemonDB.competences.size > 2) {
+        itemView.tv_competence_3.text = "‣ " + pokemonDB.competences[2]?.nom }
 
         itemView.seek_bar_attaque.progress = pokemonDB.attaque
         itemView.seek_bar_defense.progress = pokemonDB.defense
         itemView.seek_bar_speciale_attaque.progress = pokemonDB.attaqueSpeciale
         itemView.seek_bar_speciale_defense.progress = pokemonDB.defenseSpeciale
+
+        lockSeekBar(itemView.seek_bar_attaque)
+        lockSeekBar(itemView.seek_bar_defense)
+        lockSeekBar(itemView.seek_bar_speciale_attaque)
+        lockSeekBar(itemView.seek_bar_speciale_defense)
     }
 
     companion object {
